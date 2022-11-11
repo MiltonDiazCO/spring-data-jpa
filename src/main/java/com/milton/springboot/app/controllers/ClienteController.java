ee5ddcb30ee6ebc74e4c67c61d3fe2b9b4361cbe
MiltonDiazCO <diazmartinezmilton@gmail.com>
2022-11-10 19:54:23 -0500

    Agregando funcionalidades de editar y eliminar clientes

--- src/main/java/com/milton/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/milton/springboot/app/controllers/ClienteController.java
@@ -9,14 +9,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.milton.springboot.app.models.dao.IClienteDao;
 import com.milton.springboot.app.models.entity.Cliente;
 
 @Controller
 @RequestMapping("/clientes")
+@SessionAttributes("cliente")
 public class ClienteController {
 	
 	private final String usuariosTitulo = "Clientes";
@@ -41,7 +45,7 @@ public class ClienteController {
 	}
 	
 	@PostMapping({"/form"})
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
 		
 		if(result.hasErrors()) {
 			model.addAttribute("titulo", "Agregar nuevo cliente");
@@ -49,6 +53,32 @@ public class ClienteController {
 		}
 		
 		clienteDao.save(cliente);
+		status.setComplete();
+		return "redirect:/clientes/listar";
+	}
+	
+	@GetMapping({"/form/{id}"})
+	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+		
+		Cliente cliente = null;
+		
+		if(id > 0) {
+			cliente = clienteDao.findOne(id);
+		} else {
+			return "redirect:/clientes/listar";
+		}
+		
+		model.put("cliente", cliente);
+		model.put("titulo", "Editar cliente");
+		return "clientes/form";
+	}
+	
+	@RequestMapping({"/eliminar/{id}"})
+	public String eliminar(@PathVariable(value="id") Long id) {
+		
+		if(id > 0) {
+			clienteDao.delete(id);
+		}
 		return "redirect:/clientes/listar";
 	}
 
