9ae0be54f0ee2ad3faa7cb55239ad0f70becf8fc
MiltonDiazCO <diazmartinezmilton@gmail.com>
2023-02-03 17:43:08 -0500

    Creacion de las clases entity

---
+++ src/main/java/com/milton/springboot/app/models/entity/ItemFactura.java
@@ -0,0 +1,64 @@
+package com.milton.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="items_factura")
+public class ItemFactura implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer cantidad;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Producto producto;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Factura factura;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public Factura getFactura() {
+		return factura;
+	}
+
+	public void setFactura(Factura factura) {
+		this.factura = factura;
+	}
+
+}
